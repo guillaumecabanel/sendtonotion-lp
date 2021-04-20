d85ee58862bc8554d99a76a5f579a308617784d4
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2021-04-20 13:13:41 +0200

    Update build directory name
    add illustration

--- build.rb
+++ build.rb
@@ -3,14 +3,14 @@ require 'fileutils'
 html_file = "index.html"
 style_file = "style.css"
 
-FileUtils.mkdir("dist") unless File.exist?("dist")
+FileUtils.mkdir("site") unless File.exist?("site")
 
 [html_file, style_file].each do |file|
-  File.delete("dist/#{file}") if File.exist?("dist/#{file}")
-  FileUtils.cp(file, "dist/#{file}")
+  File.delete("site/#{file}") if File.exist?("site/#{file}")
+  FileUtils.cp("src/#{file}", "site/#{file}")
 end
 
-style = File.read("dist/#{style_file}")
+style = File.read("site/#{style_file}")
 
 minified_style = style.gsub(/\/.*\//, "")
 minified_style = minified_style.gsub(/:\s/, ":")
@@ -19,12 +19,11 @@ minified_style = minified_style.gsub(/\n/, "")
 minified_style = minified_style.gsub(/\s\s/, "")
 minified_style = minified_style.gsub(/;}/, "}")
 
-File.open("dist/#{style_file}", "w") { |file| file.puts minified_style }
+File.open("site/#{style_file}", "w") { |file| file.puts minified_style }
 
-html = File.read("dist/#{html_file}")
+html = File.read("site/#{html_file}")
 
 minified_html = html.gsub(/\n/, "")
 minified_html = minified_html.gsub(/>\s*</, "><")
 
-
-File.open("dist/#{html_file}", "w") { |file| file.puts minified_html }
+File.open("site/#{html_file}", "w") { |file| file.puts minified_html }
